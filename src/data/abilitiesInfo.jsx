e978a41190b9aba41333db37bf2637eabde5fbf9
Amy Wong <akwong24@outlook.com>
2025-10-06 23:03:25 -0700

    Bibi

--- src/data/abilitiesInfo.jsx
+++ src/data/abilitiesInfo.jsx
@@ -53,30 +53,56 @@ export const starPowerDescriptions = {
     23000143: "Carrion Crow - Crow deals +40% damage with his attack and Super to targets with 50% or less health.",
 
     // Poco
+    23000089: "Da Capo! - When Poco's attack hits friendly Brawlers they now heal for 962 health.",
+    23000144: "Screeching Solo - Poco's Super now also hits enemies, dealing 1520 damage.",
 
     // Bo
+    23000090: "Circling Eagle - Bo spots enemies hidden in bushes from 150% longer distance than normally.",
+    23000148: "Snare a Bear Instead of a knockback, Bo's traps now stun the enemy for 2.0 seconds!",
 
     // Piper
+    23000091: "Ambush - Piper's attack deals +1190 extra damage (at max range) when she's hidden in a bush.",
+    23000152: "Snappy Sniping - When Piper hits an enemy with her attack, she reloads 0.4 ammo instantly.",
 
     // Pam
+    23000092: "Mama's Hug - Whenever Pam hits enemies with Scrapstorm, she heals herself and nearby friendly Brawlers for 96 health.",
+    23000139: "Mama's Squeeze - Healing turret now also damages enemies for 960 damage per second.",
 
     // Tara
+    23000093: "Black Portal - Tara's Super cracks open a dimensional portal! A shadowy version of Tara appears and attacks her enemies.",
+    23000160: "Healing Shade - Tara's Super cracks open a dimensional portal! A shadowy version of Tara appears to heal Tara and her teammates.",
 
     // Darryl
-    
+    23000094: "Steel Hoops - After Barrel Roll all damage Darryl takes is reduced by 25% for 2 seconds.",
+    23000157: "Rolling Reload - When Darryl uses his Super, he doubles his reload speed for 5.0 seconds.",
+
     // Penny
+    23000099: "Heavy Coffers - Penny's gold pouches burst wtih a 100% wider cone after each hit.",
+    23000142: "Master Blaster - Penny's cannon deals 1372 damage and knocks back nearby enemies on landing.",
 
     // Frank
+    23000104: "Power Grab - Frank steals the power of a Brawler he defeats, increasing his damage by 25% for 12 seconds!",
+    23000153: "Sponge - Frank gains +1340 health.",
 
     // Gene
+    23000109: "Magic Puffs - Gene heals all friendly Brawlers around him for 576 health per second.",
+    23000159: "Spirit Slap - Gene's Magic Hand also damages enemies, dealing 2000 damage on hit.",
 
     // Tick
+    23000114: "Well Oiled - When Tick takes no damage and doesn't attack, he starts recovering health 2.0 seconds faster than normal.",
+    23000161: "Automa-Tick Reload - Tick's reload time is 9% shorter.",
 
     // Leon
+    23000119: "Smoke Trails - When Leon uses his Super, he gains a 30% boost to his movement speed for the duration of his invisibility.",
+    23000141: "Invisiheal - Leon recovers 1360 health per second while his Super is active.",
 
     // Rosa
+    23000124: "Plant Life - Rosa recovers 300 health per second when inside a bush.",
+    23000147: "Thorny Gloves - Rosa's punches gain +276 damage during her Super.",
 
     // Carl
+    23000129: "Power Throw - Carl throws his Pickaxe with 10% more speed, allowing it to travel faster and return faster.",
+    23000145: "Protective Pirouette - During Carl's Super, all damage he receives is reduced by 35%.",
 
     // Bibi
 
@@ -274,30 +300,56 @@ export const gadgetDescriptions = {
     23000286: "Slowing Toxin - All currently poisoned enemies are slowed for 2 seconds.",
 
     // Poco
+    23000267: "Tuning Fork -Poco and all nearby allies heal 740 health per second for 5 seconds.",
+    23000350: "Protective Tunes - Removes active adverse effects from friendly brawlers in a large area and gives a 4.0 second immunity.",
 
     // Bo
+    23000263: "Super Totem - Bo places a totem that increases allies' Super charging rate by 50%. The totem slowly loses its health over time.",
+    23000289: "Tripwire - Bo triggers all of his mines after 1.5 seconds. During the delay the mines are completely undetectable to opponents.",
 
     // Piper
+    23000268: "Auto Aimer - Piper pops a defensive shot at the closest enemy, dealing 170 damage, while also pushing them back.",
+    23000291: "Homemade Recipe - On activation, Piper's next main attack will home in on enemies.",
 
     // Pam
+    23000257: "Pulse Modulator - Pam triggers her turret to emit a pulse that heals allies inside the turret's range, including herself, for 1920 health.",
+    23000404: "Scrapsucker - Each hit from Pam's next attack removes 25% of opposing Brawler's maximum ammo. Pam reclaims 50% of the ammo for herself.",
 
     // Tara
+    23000266: "Psychic Enhancer - Tara and her allies are able to see all enemies, even inside bushes, for 4.0 seconds.",
+    23000356: "Support from Beyond - Tara surrounds herself with three weak shadows that attack enemies and disappear after 6 seconds.",
 
     // Darryl
+    23000260: "Recoiling Rotator - Darryl spins around and sprays a barrage of shots in all directions. Each shot deals 600 damage and recharges his Super by 25% if it hits enemies!",
+    23000313: "Tar Barrel - Darryl creates a slowing area around himself for 5.0 seconds.",
     
     // Penny
+    23000248: "Salty Barrel - Penny drops a barrel of salt that blocks enemy projectiles. She can also use it to burst her own gold pouches.",
+    23000287: "Trusty Spyglass - Penny's cannon blasts at every visible enemy position within range.",
 
     // Frank
+    23000261: "Active Noise Canceling - Frank clears any disabling effect on himself, and momentarily becomes immune to stuns, slows and knockbacks.",
+    23000351: "Irresistible Attraction - Frank's next attack deals an extra 50% damage and pulls opponents towards him.",
 
     // Gene
+    23000252: "Lamp Blowout - All enemies close to Gene are instantly pushed back. If at least one enemy Brawler is within range, Gene will also restore 10% of his max health.",
+    23000352: "Vengeful Spriits - Gene shoots a homing missile at all visible enemies within a large area, dealing up to 1600 damage based on distance.",
 
     // Tick
+    23000253: "Mine Mania - Tick's next attack fires 6 mines.",
+    23000355: "Last Hurrah - Tick gains a 50% shield for 1.0 seconds. After that, it bursts out for 1280 damage.",
 
     // Leon
+    23000276: "Clone Projector - Leon creates an illusion of himself to confuse his enemies.",
+    23000408: "Lollipop Drop - Leon creates a stealthy area for his team to hide in. The lollipop slowly loses its health over time.",
 
     // Rosa
+    23000242: "Grow Light - Rosa fertilizes the ground around her and bushes instantly grow to provide great cover.",
+    23000348: "Unfriendly Bushes - All opponents hiding in bushes take 276 damage and get slowed down for 3 seconds.",
 
     // Carl
+    23000262: "Heat Ejector - The next pickaxe Carl throws will drop a trail of hot rocks behind it! Opponents that touch them are set on fire, and receive 600 damage per second.",
+    23000317: "Flying Hook - Carl's next attack makes his pickaxe pull him to the farthest point of the attack.",
 
     // Bibi
 
