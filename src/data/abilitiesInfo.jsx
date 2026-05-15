d148227f24a3da4958bbbc425e4a92e5cc048759
Amy Wong <akwong24@outlook.com>
2026-05-15 01:11:59 -0700

    updated site to add Damian, Starr Nova, and Bolt. still need to change file names for star powers

--- src/data/abilitiesInfo.jsx
+++ src/data/abilitiesInfo.jsx
@@ -1,92 +1,92 @@
 // Star Power descriptions
 export const starPowerDescriptions = {
     // Shelly
-    23000076: "Shell Shock - Shelly's Super shells slow down enemies for 2 seconds!",
-    23000135: "Band-Aid - When Shelly falls below 40% health, she instantly heals for 2220 health. Band-Aid recharges in 15 seconds.",
+    23000076: "Shelly's Super shells slow down enemies for 2 seconds!",
+    23000135: "When Shelly falls below 40% health, she instantly heals for 2220 health. Band-Aid recharges in 15 seconds.",
 
     // Colt
-    23000077: "Slick Boots - Colt's movement speed is increased by 13%.",
-    23000138: "Magnum Special - Colt's attack range and bullet speed are increased by 11%.",
+    23000077: "Colt's movement speed is increased by 13%.",
+    23000138: "Colt's attack range and bullet speed are increased by 11%.",
 
     // Bull
-    23000078: "Berserker - When Bull falls below 60% health, his reload speed doubles!",
-    23000137: "Tough Guy - When Bull falls below 40% health, he gains a shield that reduces all damage he takes by 30%.",
+    23000078: "When Bull falls below 60% health, his reload speed doubles!",
+    23000137: "When Bull falls below 40% health, he gains a shield that reduces all damage he takes by 30%.",
 
     // Brock
-    23000079: "More Rockets! - Rocket Rain now rains down 44% more rockets.",
-    23000150: "Rocket No. 4 - Brock loads a fourth rocket into his launcher, increasing his ammo capacity (and discarding the thermos bottle he's been keeping in his launcher's #4 tube).",
+    23000079: "Rocket Rain now rains down 44% more rockets.",
+    23000150: "Brock loads a fourth rocket into his launcher, increasing his ammo capacity (and discarding the thermos bottle he's been keeping in his launcher's #4 tube).",
 
     // Rico
-    23000080: "Super Bouncy - Rico's bullets, from attack and Super alike, get supercharged by their first bounce and deal +256 damage!",
-    23000156: "Robo Retreat - When Rico falls below 40% health, he runs 34% faster!",
+    23000080: "Rico's bullets, from attack and Super alike, get supercharged by their first bounce and deal +256 damage!",
+    23000156: "When Rico falls below 40% health, he runs 34% faster!",
 
     // Spike
-    23000081: "Fertilize - After using Super, Spike regenerates 1400 health per second by staying in its area of effect.",
-    23000151: "Curveball - Spikes from the cactus grenade fly in a curving motion, making it easier to hit targets.",
+    23000081: "After using Super, Spike regenerates 1400 health per second by staying in its area of effect.",
+    23000151: "Spikes from the cactus grenade fly in a curving motion, making it easier to hit targets.",
 
     // Barley
-    23000082: "Medical Use - Barley regains 480 health from each attack.",
-    23000158: "Extra Noxious - Adds +200 damage per second to Barley's attack.",
+    23000082: "Barley regains 480 health from each attack.",
+    23000158: "Adds +200 damage per second to Barley's attack.",
 
     // Jessie
-    23000083: "Energize - Jessie can repair her gun turret for 1060 of its missing health by zapping it with her attack.",
-    23000149: "Shocky - Scrappy the Turret now shoots energy orbs that bounce between enemies. The orbs' range after a bounce is 51% of normal range.",
+    23000083: "Jessie can repair her gun turret for 1060 of its missing health by zapping it with her attack.",
+    23000149: "Scrappy the Turret now shoots energy orbs that bounce between enemies. The orbs' range after a bounce is 51% of normal range.",
 
     // Nita
-    23000084: "Bear With Me - Nita recovers 1040 health whenever her bear hits an enemy brawler. When Nita deals damage to an enemy brawler, her bear regains 1040 health.",
-    23000136: "Hyper Bear - Nita's bear attacks faster. Time between swipes is reduced by 60%.",
+    23000084: "Nita recovers 1040 health whenever her bear hits an enemy brawler. When Nita deals damage to an enemy brawler, her bear regains 1040 health.",
+    23000136: "Nita's bear attacks faster. Time between swipes is reduced by 60%.",
 
     // Dynamike
-    23000085: "Dyna-Jump - Dynamike can ride the blast wave of his explosives to jump over obstacles!",
-    23000155: "Demolition - Adds +1600 damage to Mike's Super.",
+    23000085: "Dynamike can ride the blast wave of his explosives to jump over obstacles!",
+    23000155: "Adds +1600 damage to Mike's Super.",
 
     // El Primo
-    23000086: "El Fuego - Enemies caught in El Primo's Super will burn for 1800 damage over 4 seconds.",
-    23000140: "Meteor Rush - El Primo gains a 25% speed boost for 4.0 seconds after using his Super.",
+    23000086: "Enemies caught in El Primo's Super will burn for 1800 damage over 4 seconds.",
+    23000140: "El Primo gains a 25% speed boost for 4.0 seconds after using his Super.",
 
     // Mortis
-    23000087: "Creepy Harvest - Mortis reaps the life essence of a Brawler he defeats, restoring 20% of his max health.",
-    23000154: "Coiled Snake - Mortis gains his longer dash 2 seconds faster.",
+    23000087: "Mortis reaps the life essence of a Brawler he defeats, restoring 20% of his max health.",
+    23000154: "Mortis gains his longer dash 2 seconds faster.",
 
     // Crow
-    23000088: "Extra Toxic - Crow's poison saps the strength of enemies, who deal 15% less damage while poisoned.",
-    23000143: "Carrion Crow - Crow deals +40% damage with his attack and Super to targets with 50% or less health.",
+    23000088: "Crow's poison saps the strength of enemies, who deal 15% less damage while poisoned.",
+    23000143: "Crow deals +40% damage with his attack and Super to targets with 50% or less health.",
 
     // Poco
-    23000089: "Da Capo! - When Poco's attack hits friendly Brawlers they now heal for 962 health.",
-    23000144: "Screeching Solo - Poco's Super now also hits enemies, dealing 1520 damage.",
+    23000089: "When Poco's attack hits friendly Brawlers they now heal for 962 health.",
+    23000144: "Poco's Super now also hits enemies, dealing 1520 damage.",
 
     // Bo
-    23000090: "Circling Eagle - Bo spots enemies hidden in bushes from 150% longer distance than normally.",
-    23000148: "Snare a Bear Instead of a knockback, Bo's traps now stun the enemy for 2.0 seconds!",
+    23000090: "Bo spots enemies hidden in bushes from 150% longer distance than normally.",
+    23000148: "Instead of a knockback, Bo's traps now stun the enemy for 2.0 seconds!",
 
     // Piper
-    23000091: "Ambush - Piper's attack deals +1190 extra damage (at max range) when she's hidden in a bush.",
-    23000152: "Snappy Sniping - When Piper hits an enemy with her attack, she reloads 0.4 ammo instantly.",
+    23000091: "Piper's attack deals +1190 extra damage (at max range) when she's hidden in a bush.",
+    23000152: "When Piper hits an enemy with her attack, she reloads 0.4 ammo instantly.",
 
     // Pam
-    23000092: "Mama's Hug - Whenever Pam hits enemies with Scrapstorm, she heals herself and nearby friendly Brawlers for 96 health.",
-    23000139: "Mama's Squeeze - Healing turret now also damages enemies for 960 damage per second.",
+    23000092: "Whenever Pam hits enemies with Scrapstorm, she heals herself and nearby friendly Brawlers for 96 health.",
+    23000139: "Healing turret now also damages enemies for 960 damage per second.",
 
     // Tara
-    23000093: "Black Portal - Tara's Super cracks open a dimensional portal! A shadowy version of Tara appears and attacks her enemies.",
-    23000160: "Healing Shade - Tara's Super cracks open a dimensional portal! A shadowy version of Tara appears to heal Tara and her teammates.",
+    23000093: "Tara's Super cracks open a dimensional portal! A shadowy version of Tara appears and attacks her enemies.",
+    23000160: "Tara's Super cracks open a dimensional portal! A shadowy version of Tara appears to heal Tara and her teammates.",
 
     // Darryl
-    23000094: "Steel Hoops - After Barrel Roll all damage Darryl takes is reduced by 25% for 2 seconds.",
-    23000157: "Rolling Reload - When Darryl uses his Super, he doubles his reload speed for 5.0 seconds.",
+    23000094: "After Barrel Roll all damage Darryl takes is reduced by 25% for 2 seconds.",
+    23000157: "When Darryl uses his Super, he doubles his reload speed for 5.0 seconds.",
 
     // Penny
-    23000099: "Heavy Coffers - Penny's gold pouches burst wtih a 100% wider cone after each hit.",
-    23000142: "Master Blaster - Penny's cannon deals 1372 damage and knocks back nearby enemies on landing.",
+    23000099: "Penny's gold pouches burst wtih a 100% wider cone after each hit.",
+    23000142: "Penny's cannon deals 1372 damage and knocks back nearby enemies on landing.",
 
     // Frank
-    23000104: "Power Grab - Frank steals the power of a Brawler he defeats, increasing his damage by 25% for 12 seconds!",
-    23000153: "Sponge - Frank gains +1340 health.",
+    23000104: "Frank steals the power of a Brawler he defeats, increasing his damage by 25% for 12 seconds!",
+    23000153: "Frank gains +1340 health.",
 
     // Gene
-    23000109: "Magic Puffs - Gene heals all friendly Brawlers around him for 576 health per second.",
-    23000159: "Spirit Slap - Gene's Magic Hand also damages enemies, dealing 2000 damage on hit.",
+    23000109: "Gene heals all friendly Brawlers around him for 576 health per second.",
+    23000159: "Gene's Magic Hand also damages enemies, dealing 2000 damage on hit.",
 
     // Tick
     23000114: "Well Oiled - When Tick takes no damage and doesn't attack, he starts recovering health 2.0 seconds faster than normal.",
@@ -395,53 +395,73 @@ export const starPowerDescriptions = {
     // Glowbert
     23001181: "Biotic Ecosystem - If Glowbert is tethered to an enemy and teammate at the same time, it will reduce enemy damage by 20% and increase the teammates damage by 20%.",
     23001182: "Parasitism - Tethering to an enemy also heals Glowbert for 50% of the damage dealt.",
+
+    // Sirius
+    23001189: "Shadows have +15% movement speed.",
+    23001190: "Each hit with a Shadow Strike counts as 2 to collect Brawler Shadows, instead of 1.",
+
+    // Najia
+    23001216: "Brawlers defeated while poisoned by Najia will spawn a snake in their place.",
+    23001217: "Poison damage scales with the target Brawler's current percent health, dealing up to an extra 200 damage when a target is at 100% health.",
+
+    // Damian
+    23001245: "Knocking a target onto a wall stuns them for 0.5 seconds.",
+    23001246: "Damian has a flaming fist during his Super, which burns enemies.",
+
+    // Starr Nova
+    23001253: "Landing attacks in Sword Form increases its damage by 5% up to 30%.",
+    23001254: "Starr Nova's main attack heals teammates equal to 30% of her damage.",
+
+    // Bolt
+    23001279: "Bolt will knock all enemies upwards when he's at full speed.",
+    23001280: "Bolt is immune to crowd control and gains 15% top speed during his Super.",
 };
 
 // Gadget descriptions
 export const gadgetDescriptions = {
     // Shelly
-    23000255: "Fast Forward - Shelly dashes ahead, skipping a few unnecessary steps and fully reloading her main attack!",
-    23000288: "Clay Pigeons - On activation, for the next 5 seconds Shelly's main attacks focuses the fire to a smaller area with an increased range.",
+    23000255: "Shelly dashes ahead, skipping a few unnecessary steps and fully reloading her main attack!",
+    23000288: "On activation, for the next 5 seconds Shelly's main attacks focuses the fire to a smaller area with an increased range.",
 
     // Colt
-    23000273: "Speedloader - Colt instantly reloads 2 ammo.",
-    23000319: "Silver Bullet - Colt's next attack is a powerful shot that deals as much damage as 2 of his regular bullets, while going through obstacles and opponents alike.",
+    23000273: "Colt instantly reloads 2 ammo.",
+    23000319: "Colt's next attack is a powerful shot that deals as much damage as 2 of his regular bullets, while going through obstacles and opponents alike.",
 
     // Bull
-    23000272: "T-Bone Injector - Bull instantly rejuvenates himself for 2000 health.",
-    23000310: "Stomper - Bull's next Super will stun any opponent they hit for 0.5 seconds.",
+    23000272: "Bull instantly rejuvenates himself for 2000 health.",
+    23000310: "Bull's next Super will stun any opponent they hit for 0.5 seconds.",
 
     // Brock
-    23000245: "Rocket Laces - Brock blasts the ground below him and propels himself into the air. The explosion deals 580 damage to nearby enemies.",
+    23000245: "Brock blasts the ground below him and propels himself into the air. The explosion deals 580 damage to nearby enemies.",
     23000316: "Brock's next attack is a mega rocket that is bigger, badder, faster and destroys walls.",
 
     // Rico
-    23000246: "Multiball Launcher - Rico blasts waves of bouncy bullets in all directions.",
-    23000409: "Bouncy Castle - Rico's next attack will heal him for 256 health from each projectile bounce.",
+    23000246: "Rico blasts waves of bouncy bullets in all directions.",
+    23000409: "Rico's next attack will heal him for 256 health from each projectile bounce.",
 
     // Spike
-    23000247: "Popping Pincushion - Spike shoots 3 waves of needles in all directions, dealing 1080 damage per hit.",
-    23000406: "Life Plant - Spike grows a large catcus with 2800 health to giver cover to friendlies. If destroyed, the catcus bursts, and all nearby friendlies recover 1680 health.",
+    23000247: "Spike shoots 3 waves of needles in all directions, dealing 1080 damage per hit.",
+    23000406: "Spike grows a large catcus with 2800 health to giver cover to friendlies. If destroyed, the catcus bursts, and all nearby friendlies recover 1680 health.",
 
     // Barley
-    23000250: "Sticky Syrup Mixer - Barley drops a sticky concoction that leaves a puddle, slowing down all enemies that makes contact with it.",
-    23000293: "Herbal Tonic - Barley throws a healing potion at nearby allies that creates an area that heals for 720 health per second.",
+    23000250: "Barley drops a sticky concoction that leaves a puddle, slowing down all enemies that makes contact with it.",
+    23000293: "Barley throws a healing potion at nearby allies that creates an area that heals for 720 health per second.",
 
     // Jessie
-    23000251: "Spark Plug - Jessie triggers a shockwave from her turret, slowing down all enemies within its area of effect.",
-    23000295: "Recoil Spring = Scrappy's attack speed is doubled for 5.0 seconds.",
+    23000251: "Jessie triggers a shockwave from her turret, slowing down all enemies within its area of effect.",
+    23000295: "Scrappy's attack speed is doubled for 5.0 seconds.",
 
     // Nita
-    23000249: "Bear Paws - Nita commands her bear to slam the ground, stunning all enemies within its reach.",
-    23000314: "Faux Fur - For the next 3 seconds, Nita's bear gets a 35% shield against damage.",
+    23000249: "Nita commands her bear to slam the ground, stunning all enemies within its reach.",
+    23000314: "For the next 3 seconds, Nita's bear gets a 35% shield against damage.",
 
     // Dynamike
-    23000258: "Fidget Spinner - Dynamike spins furiously with increased movement speed and throws multiple sticks of dynamite around himself. Each dynamite deals 2400 damage to enemies.",
-    23000294: "Satchel Charge - Once activated, the next main attack also stuns enemies for 1.5 seconds.",
+    23000258: "Dynamike spins furiously with increased movement speed and throws multiple sticks of dynamite around himself. Each dynamite deals 2400 damage to enemies.",
+    23000294: "Once activated, the next main attack also stuns enemies for 1.5 seconds.",
 
     // El Primo
-    23000264: "Suplex Supplement - El Primo grabs the closest enemy within his reach and flips them like a pancake over his broad shoulders.",
-    23000292: "Asteroid Belt - El Primo summons a small meteor to strike the nearest enemy. It deals 3040 damage and destroys walls.",
+    23000264: "El Primo grabs the closest enemy within his reach and flips them like a pancake over his broad shoulders.",
+    23000292: "El Primo summons a small meteor to strike the nearest enemy. It deals 3040 damage and destroys walls.",
 
     // Mortis
     23000265: "Combo Spinner - Mortis spins his shovel, hitting all enemies around himself for 2000 damage.",
@@ -764,8 +784,8 @@ export const gadgetDescriptions = {
     23000932: "Time For a Slow Song - Jae-yong slows enemies around him and his attacks will leave trails of slowing paperwork for 5 sec.",
 
     // Kaze
-    23000940: "Gracious Host - Switch to Ninja Aspect and gain 30% Movement Speed for 2.5 sec. \nSwitch to Geisha Aspect and heal for 30% of max health.",
-    23000941: "Hensojutsu - Switch to Ninja Aspect and gain invisibility for 2 sec. \nSwitch to Geisha Aspect and dash forward.",
+    23000940: "Gracious Host - Switch to Ninja Aspect and gain 30% Movement Speed for 2.5 sec. Switch to Geisha Aspect and heal for 30% of max health.",
+    23000941: "Hensojutsu - Switch to Ninja Aspect and gain invisibility for 2 sec. Switch to Geisha Aspect and dash forward.",
 
     // Alli
     23000991: "Feed the Gators - The next main attack heals Alli for 100% of the damage dealt.",
@@ -794,6 +814,27 @@ export const gadgetDescriptions = {
     // Glowbert
     23001183: "Slippery Savior - Glowbert dashes in a target direction, healing himself and teammates on arrival 2500 HP on arrival. Cooldown: 13 seconds.",
     23001184: "More Lumens! - Glowbert supercharges his glowbeam tethers, doubling damage/healing for 2.5 secs. Cooldown: 17 seconds.",
+
+    // Sirius
+    23001191: "Sirius fires a projectile that summons a Shadow on hit, also slowing the hit target by 20% for 2 sec. Cooldown: 20 seconds.",
+    23001192: "Summoned Shadows return to Sirius, who heals them for 25% of their health. Cooldown: 13 seconds.",
+
+    // Najia
+    23001218: "All enemies currently poisoned by Najia will create poisonous puddles for the next 2 sec. Cooldown: 17 seconds.",
+    23001219: "Najia hides inside a vase. Once the vase is broken, she will knock back any nearby enemies! Cooldown: 20 seconds.",
+
+    // Damian
+    23001247: "Damian throws his microphone to a target location. The first teammate to pick it up is healed for 3200 health. Cooldown: 15 seconds.",
+    23001248: "Damian summons a temporary wall of indestructible amps! Cooldown: 20 seconds.",
+
+    // Starr Nova
+    23001255: "Starr Nova deploys an anti-gravity device, allowing teammates to float inside of the area! Cooldown: 20 seconds.",
+    23001256: "Starr Nova fires an energy ball that damages enemies and heals teammates for 1600. This energy ball can also be teleported to before it hits a target! Cooldown: 20 seconds.",
+
+    // Bolt
+    23001281: "Bolt gains a 2000 - 4000 health shield based on his current speed. Cooldown: 15 seconds.",
+    23001282: "Bolt jumps to a target location and deals area damage. Breaks environments but loses all speed when landing. Cooldown: 20 seconds.",
+
 };
 
 // Helper function to get description with fallback
